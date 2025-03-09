7adb72a1fc9a2ab482007ab9e4a06df87b4c4e3b
Christina Voss <chustvedt@gmail.com>
2025-03-09 13:00:41 +0000

    fix safety issue

--- app/controllers/hives_controller.rb
+++ app/controllers/hives_controller.rb
@@ -2,9 +2,7 @@ class HivesController < ApplicationController
   load_and_authorize_resource
 
   def show
-    puts "INSIDE HIVES CONTROLLER - SHOW"
     hive = current_user.hive
-    pp hive
     map = MapComponent.new(location: hive.location)
     render BlankPageComponent.new(component: map)
   end
@@ -14,13 +12,21 @@ class HivesController < ApplicationController
     lat = params[:lat].to_f
     lng = params[:lng].to_f
 
-  # Calculate the distance from the current user's hive to the given coordinates
-  distance = ActiveRecord::Base.connection.execute(
-    "SELECT ST_Distance(
-      '#{hive.location}'::geography,
-      ST_SetSRID(ST_MakePoint(#{lng}, #{lat}), 4326)::geography
-    ) / 1000 AS distance"
-  ).first["distance"]
+    factory = RGeo::Geographic.spherical_factory(srid: 4326)
+    hive_location = factory.point(hive.location.x, hive.location.y)
+    bee_location = factory.point(lng, lat)
+
+    # Safely quote the hive location and click location
+    quoted_hive_location = Hive.connection.quote(hive_location)
+    quoted_bee_location = Hive.connection.quote(bee_location)
+
+    # Calculate the distance from the current user's hive to the given coordinates
+    distance = Hive.connection.exec_query(
+      "SELECT ST_Distance(
+        #{quoted_hive_location}::geography,
+        #{quoted_bee_location}::geography
+      ) / 1000 AS distance"
+    ).first["distance"]
 
     bee_flight_time = (distance / User::BEE_SPEED_TO_HIVE)  * 60 * 60 # Convert hours to seconds
 
