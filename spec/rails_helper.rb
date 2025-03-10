4b335df6bbdfb2c5dc767234deef38291ff96407
Christina Voss <chustvedt@gmail.com>
2025-03-10 15:50:17 +0000

    rubocopped

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,7 +13,7 @@ require 'rspec/rails'
 require 'capybara/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 # Add additional requires below this line. Rails is not loaded until this point!
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Rails.root.glob('spec/support/**/*.rb').each { |f| require f }
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module AuthenticationHelpers
   def sign_in(user)
     visit new_session_path
