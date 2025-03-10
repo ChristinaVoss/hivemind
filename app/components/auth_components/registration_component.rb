f7ff5c2cb5def74f73d3258343a00f66629a44c9
Christina Voss <chustvedt@gmail.com>
2025-03-10 14:51:43 +0000

    Move registration component

---
+++ app/components/auth_components/registration_component.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module AuthComponents
+  class RegistrationComponent < ViewComponent::Base
+    BASE_CLASS = 'registration'
+    attr_reader :user, :attrs
+
+    def initialize(user: nil, attrs: {})
+      classes = [BASE_CLASS]
+      classes += Array(attrs[:class])
+
+      @user = user
+      @attrs = attrs.merge(class: classes)
+
+      super
+    end
+  end
+end
--- lib/component_previews/registration_component_preview.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class RegistrationComponentPreview < ViewComponent::Preview
-  def default
-    render(RegistrationComponent.new(user: User.new))
-  end
-end
