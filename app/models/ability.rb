76943d7c0e69283261a388e1f6dbe26d6c46355f
Christina Voss <chustvedt@gmail.com>
2025-03-16 10:59:42 +0000

    Fix user check

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,7 +6,7 @@ class Ability
   def initialize(user)
     # for testing only - remove soon!
     # can :read, :all
-    return unless user.present? # Guests have no permissions
+    return if user.blank? # Guests have no permissions
 
     # A bee can manage (create, update, delete, read) its own hive
     can :manage, Hive, user_id: user.id
